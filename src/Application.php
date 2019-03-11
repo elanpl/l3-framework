a65428c87824645ea76a8df0486460f6f36d0d4f
Jacek Chwastek <jacek.chwastek@e-lan.pl>
2019-03-11 12:54:18 +0100

    PSR config loading

--- src/Application.php
+++ src/Application.php
@@ -2,22 +2,23 @@
 
 namespace elanpl\L3;
 
-class Application{
+class Application
+{
     public $Request; // build from received HTTP request
     public $Response; // response to be send
     public $Router; // routing engine
     public $Serialization; // serialization engine
     public $BaseDirectory; // directory root of the application
     public $ConfigDirectory; // directory containing application configuration files
-    public $ControllersDirectory; // subdirectory/namespace part added to controller class 
+    public $ControllersDirectory; // subdirectory/namespace part added to controller class
     public $ViewsDirectory; // subdirectory to search for the view files
     public $ApplicationDirectory; // subdirectory/namespace part of the application
     //public $Controller; //Active controller
     //public $Module; //Active module
     //public $Action; //Active action
-    public $Config;
 
-    public function __construct(){
+    public function __construct()
+    {
         //create request object
         $this->Request = new Request();
         //create response objcet
@@ -25,38 +26,40 @@ class Application{
         //create router object
         $this->Router = new Router();
         //create serialization object
-        $this->Serialization = new Serialization(); 
+        $this->Serialization = new Serialization();
     }
 
-    public function basePath(){
+    public function basePath()
+    {
         return strstr($_SERVER['REQUEST_URI'], $this->Request->Path, true);
     }
 
-    public function relPath($file){
-        return ($this->Router->depth>0 ? str_repeat("../", substr($this->Request->Path,-1)=='/'?$this->Router->depth:$this->Router->depth-1) : "" ).$file; 
+    public function relPath($file)
+    {
+        return ($this->Router->depth > 0 ? str_repeat('../', substr($this->Request->Path, -1) == '/' ? $this->Router->depth : $this->Router->depth - 1) : '').$file;
     }
 
-    public function link($name, $parameters){
+    public function link($name, $parameters)
+    {
         return $this->Router->link($name, $parameters);
     }
 
-    public function LoadApplicationConfiguration(){
-        //include routing configuration
-        include_once($this->ConfigDirectory."routing.php");
-        //include the application configuration
-        include_once($this->ConfigDirectory.'app.php');
-
-        $this->Config = require($this->ConfigDirectory."config.php");
+    public function LoadApplicationConfiguration($routing = null, $app = null)
+    {
+        $routing();
+        $app();
     }
 
-    public function LoadConfigFile($file){
+    public function LoadConfigFile($file)
+    {
         //include configuration file
-        return include_once($this->ConfigDirectory.$file);
+        return include_once $this->ConfigDirectory.$file;
     }
 
-    public function IncludeFromConfigDir($file){
+    public function IncludeFromConfigDir($file)
+    {
         //include configuration file
-        return include($this->ConfigDirectory.$file);
+        return include $this->ConfigDirectory.$file;
     }
 
     /*
@@ -65,23 +68,24 @@ class Application{
         include_once(_L3_CONTROLLER_PATH.$Controller.'.php');
     }*/
 
-    public function FindControllerInCallStack($search_level=5){
+    public function FindControllerInCallStack($search_level = 5)
+    {
         //search for controller, action and module if present
-        $controller = "";
-        $module = "";
-        $action = "";
+        $controller = '';
+        $module = '';
+        $action = '';
         $match = array();
 
         //analize the call stack
-          $call_stack = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $search_level);
-        
-        for($i=0; $i<$search_level; $i++){
-            if(preg_match(
+        $call_stack = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $search_level);
+
+        for ($i = 0; $i < $search_level; ++$i) {
+            if (preg_match(
                     $pattern = '#('.$this->ApplicationDirectory.')(\\\\(?P<module>.+))?\\\\'.$this->ControllersDirectory.'\\\\(?P<controller>.+)#',
                     $call_stack[$i]['class'],
                     $match
                 )
-            ){
+            ) {
                 $action = $call_stack[$i]['function'];
                 $module = $match['module'];
                 $controller = $match['controller'];
@@ -92,12 +96,12 @@ class Application{
         return array(
             'module' => $module,
             'controller' => $controller,
-            'action' => $action
+            'action' => $action,
         );
     }
 
-    function RunControllerAction($ControllerAction, $Parameters){
-        
+    public function RunControllerAction($ControllerAction, $Parameters)
+    {
         //prepare object and action name
         /*
         TODO ...
@@ -115,107 +119,103 @@ class Application{
             }
         }*/
 
-        if(is_array($ControllerAction)){
+        if (is_array($ControllerAction)) {
             //$this->IncludeControler($ControllerAction['controller']);
-            $controller_class = "\\".$this->ApplicationDirectory."\\"
-                .(isset($ControllerAction['module'])?$ControllerAction['module']."\\":"")
-                .$this->ControllersDirectory."\\"
+            $controller_class = '\\'.$this->ApplicationDirectory.'\\'
+                .(isset($ControllerAction['module']) ? $ControllerAction['module'].'\\' : '')
+                .$this->ControllersDirectory.'\\'
                 .$ControllerAction['controller'];
-            $controller_object = new $controller_class;
+            $controller_object = new $controller_class();
             $controller_action = $ControllerAction['action'];
         }
 
         //fire the action
-        if(isset($controller_object) && isset($controller_action)){
-            $this->Module = (isset($ControllerAction['module'])?$ControllerAction['module']:"");
+        if (isset($controller_object) && isset($controller_action)) {
+            $this->Module = (isset($ControllerAction['module']) ? $ControllerAction['module'] : '');
             $this->Controller = $ControllerAction['controller'];
             $this->Action = $controller_action;
             // prepare args and start the action
-            if(!isset($reflection)){
+            if (!isset($reflection)) {
                 $reflection = new \ReflectionMethod($controller_object, $controller_action);
             }
-            if($reflection->getNumberOfParameters()){
-                $fire_args=array();
+            if ($reflection->getNumberOfParameters()) {
+                $fire_args = array();
 
-                foreach($reflection->getParameters() AS $arg)
-                {
-                    if(isset($Parameters[$arg->name]) || (is_array($ControllerAction) && isset($ControllerAction['defaults'][$arg->name])))
-                        if(isset($Parameters[$arg->name]))
-                            $fire_args[$arg->name]=$Parameters[$arg->name];
-                        else
-                            $fire_args[$arg->name]=$ControllerAction['defaults'][$arg->name];
-                    else
-                    {
-                        if($arg->isDefaultValueAvailable()){
-                            $fire_args[$arg->name]=$arg->getDefaultValue();
+                foreach ($reflection->getParameters() as $arg) {
+                    if (isset($Parameters[$arg->name]) || (is_array($ControllerAction) && isset($ControllerAction['defaults'][$arg->name]))) {
+                        if (isset($Parameters[$arg->name])) {
+                            $fire_args[$arg->name] = $Parameters[$arg->name];
+                        } else {
+                            $fire_args[$arg->name] = $ControllerAction['defaults'][$arg->name];
                         }
-                        else{
-                            $fire_args[$arg->name]=null;
+                    } else {
+                        if ($arg->isDefaultValueAvailable()) {
+                            $fire_args[$arg->name] = $arg->getDefaultValue();
+                        } else {
+                            $fire_args[$arg->name] = null;
                         }
                     }
                 }
-                return $reflection->invokeArgs ($controller_object , $fire_args );
-            }
-            else{
+
+                return $reflection->invokeArgs($controller_object, $fire_args);
+            } else {
                 return $reflection->invoke($controller_object);
             }
         }
     }
 
-    public function HandleActionResult($result){
-        if(is_string($result)){
+    public function HandleActionResult($result)
+    {
+        if (is_string($result)) {
             $content = $result;
-        }
-        else if(is_object($result)){
-            if($result instanceof ViewModel){
-                if($SerializationContentType = $this->Serialization->Match($this->Request->AcceptTypes, $result)){
+        } elseif (is_object($result)) {
+            if ($result instanceof ViewModel) {
+                if ($SerializationContentType = $this->Serialization->Match($this->Request->AcceptTypes, $result)) {
                     $content = $this->Serialization->Serialize($SerializationContentType, $result);
-                    if(is_object($content)){
-                        if($content instanceof Response){
+                    if (is_object($content)) {
+                        if ($content instanceof Response) {
                             $this->Response = $content;
                             unset($content);
                         }
                     }
+                } else {
+                    echo 'Serializer not defined for Content-Type: '.$this->Request->Accept.'<br>'.PHP_EOL;
+                    throw new \Exception('Not implemented!');
                 }
-                else{
-                    echo "Serializer not defined for Content-Type: ".$this->Request->Accept."<br>".PHP_EOL;
-                    throw new \Exception("Not implemented!");
-                }
-            }
-            else if($result instanceof View){
+            } elseif ($result instanceof View) {
                 $content = $result->render();
-            }
-            else if($result instanceof Response){
+            } elseif ($result instanceof Response) {
                 $this->Response = $result;
-            }  
+            }
         }
-        if(isset($content))
-                $this->Response->withBody($content);
+        if (isset($content)) {
+            $this->Response->withBody($content);
+        }
         $this->Response->send();
     }
 
-    public function BeforeAction(){
-        if(!empty($this->Router->RouteInfo->BeforeAction)){
-            foreach($this->Router->RouteInfo->BeforeAction as $event_handler){
+    public function BeforeAction()
+    {
+        if (!empty($this->Router->RouteInfo->BeforeAction)) {
+            foreach ($this->Router->RouteInfo->BeforeAction as $event_handler) {
                 $this->Router->RouteInfo::EventHandlerFormatCheck($event_handler, $eh);
-                if(isset($eh['class'])){
-                    $object = new $eh['class'];
+                if (isset($eh['class'])) {
+                    $object = new $eh['class']();
                     $reflection = new \ReflectionMethod($object, $eh['function']);
                     $fire_args = array();
                     $fire_args[] = $this->Request;
                     $fire_args[] = $this->Router->RouteInfo;
-                    if(isset($eh['arguments'])){
+                    if (isset($eh['arguments'])) {
                         $this->Router->RouteInfo::EventHandlerArgumentsFormatCheck($eh['arguments'], $args);
                         $fire_args = array_merge($fire_args, $args);
                     }
-                    $result = $reflection->invokeArgs ($object , $fire_args );
+                    $result = $reflection->invokeArgs($object, $fire_args);
+                } elseif (is_callable($eh['function'])) {
+                    throw new \Exception('Not implemented!');
                 }
-                else if (is_callable($eh['function'])){
-                    throw new \Exception("Not implemented!");
-                }
-                if(isset($result)){
-                    if(is_object($result)){
-                        if($result instanceof Response){
+                if (isset($result)) {
+                    if (is_object($result)) {
+                        if ($result instanceof Response) {
                             $this->HandleActionResult($result);
                             exit();
                         }
@@ -225,58 +225,55 @@ class Application{
         }
     }
 
-    public function Run(){
+    public function Run()
+    {
         // Check if routing path is found
-        if($action = $this->Router->match($this->Request)){
+        if ($action = $this->Router->match($this->Request)) {
             // routing match found decide what to do next...
             // BeforeAction event
             $this->BeforeAction();
 
             // function -> call it...
-            if(is_callable($action)){
+            if (is_callable($action)) {
                 $reflection = new \ReflectionFunction($action);
-                if($reflection->getNumberOfParameters()){
-                    $fire_args=array();
-      
-                    foreach($reflection->getParameters() AS $arg)
-                    {
-                        if(isset($this->Router->parsedParameters[$arg->name]))
-                            $fire_args[$arg->name]=$this->Router->parsedParameters[$arg->name];
-                        else
-                            $fire_args[$arg->name]=null;
+                if ($reflection->getNumberOfParameters()) {
+                    $fire_args = array();
+
+                    foreach ($reflection->getParameters() as $arg) {
+                        if (isset($this->Router->parsedParameters[$arg->name])) {
+                            $fire_args[$arg->name] = $this->Router->parsedParameters[$arg->name];
+                        } else {
+                            $fire_args[$arg->name] = null;
+                        }
                     }
-                    
+
                     $result = call_user_func_array($action, $fire_args);
-                }
-                else{
+                } else {
                     $result = call_user_func($action);
                 }
-            }
-            else if(is_object($action) && is_a($action, 'L2_controler_info')){
+            } elseif (is_object($action) && is_a($action, 'L2_controler_info')) {
                 // not implemented yet.
-                throw new \Exception("Not implemented!");
-                ///$action->runControlerAction($this->routing->param);
+                throw new \Exception('Not implemented!');
+            ///$action->runControlerAction($this->routing->param);
             }
             // array with controller, and action -> run the controller action
-            else if(is_array($action) && array_key_exists('controller',$action) && array_key_exists('action', $action)){
+            elseif (is_array($action) && array_key_exists('controller', $action) && array_key_exists('action', $action)) {
                 $result = $this->RunControllerAction($action, $this->Router->parsedParameters);
-                
             }
             // string
-            else if(is_string($action)){
+            elseif (is_string($action)) {
                 $result = $action;
             }
 
             // Handle the action result
-            if(isset($result)){
+            if (isset($result)) {
                 $this->HandleActionResult($result);
             }
         }
         // routing path not found -> generate 404 response
-        else{
-            echo "Routing not found: ".$this->Request->Path."!<br>".PHP_EOL;
-            throw new \UnexpectedValueException ("Routing not found: ".$this->Request->Path."!");
+        else {
+            echo 'Routing not found: '.$this->Request->Path.'!<br>'.PHP_EOL;
+            throw new \UnexpectedValueException('Routing not found: '.$this->Request->Path.'!');
         }
- 
     }
 }
--- src/ViewEngines/TwigWrapper/TwigWrapper.php
+++ src/ViewEngines/TwigWrapper/TwigWrapper.php
@@ -2,68 +2,66 @@
 
 namespace elanpl\L3\ViewEngines\TwigWrapper;
 
-
-
-class TwigWrapper implements \elanpl\L3\ViewEngines\IViewEngine{
-
+class TwigWrapper implements \elanpl\L3\ViewEngines\IViewEngine
+{
     protected $twig;
-    
-    public function __construct($currentModule){
+
+    public function __construct($currentModule)
+    {
         global $_L3;
-        
-        $config = require( $_L3->ConfigDirectory.DIRECTORY_SEPARATOR.'twig.php' );
-        
-        $loader = new \Twig_Loader_Filesystem( );
-        
-        if ( $currentModule != '') {
+
+        $config = \config\twig::getTwigConfig();
+
+        $loader = new \Twig_Loader_Filesystem();
+
+        if ($currentModule != '') {
             $moduleViewPath = $_L3->BaseDirectory.$_L3->ApplicationDirectory.DIRECTORY_SEPARATOR
-                    .($currentModule!=""?$currentModule.DIRECTORY_SEPARATOR:"").$_L3->ViewsDirectory;
+                    .($currentModule != '' ? $currentModule.DIRECTORY_SEPARATOR : '').$_L3->ViewsDirectory;
             $loader->addPath($moduleViewPath);
-        } 
-        
+        }
+
         $baseViewPath = $_L3->BaseDirectory.$_L3->ApplicationDirectory.DIRECTORY_SEPARATOR.$_L3->ViewsDirectory;
         $loader->addPath($baseViewPath);
-         
-        $this->twig = new \Twig_Environment($loader, 
-                [ 'cache' => $config['cache'], 'debug' => $config['debug'] ]);
 
-        if( $config['debug'] ){
+        $this->twig = new \Twig_Environment($loader,
+                ['cache' => $config['cache'], 'debug' => $config['debug']]);
+
+        if ($config['debug']) {
             $this->twig->addExtension(new \Twig_Extension_Debug());
         }
-                
-        
+
         $this->twig->addFunction(
-                new \Twig_SimpleFunction ('asset', function ($string) use ($config) {
-                    return trim( $config['pageUrl'].'/'.trim($string,'/') ,'/');
+                new \Twig_SimpleFunction('asset', function ($string) use ($config) {
+                    return trim($config['pageUrl'].'/'.trim($string, '/'), '/');
                 })
             );
 
         $this->twig->addFunction(
-                new \Twig_SimpleFunction ('route', function ($string) use ($config) {
-                    return trim( $config['pageUrl'] //.\Helpers\translate::addLanguageToUrl() 
-                            .'/'.trim($string,'/') ,'/');
+                new \Twig_SimpleFunction('route', function ($string) use ($config) {
+                    return trim($config['pageUrl'] //.\Helpers\translate::addLanguageToUrl()
+                            .'/'.trim($string, '/'), '/');
                 })
-            ); 
-        
+            );
+
 //        $this->twig->addFunction(
 //                new Twig_SimpleFunction ('logged', function () {
 //                    return Auth::user();
 //                })
-//            );             
-                
+//            );
+
 //        $this->twig->addFunction(
 //                    new Twig_SimpleFunction ('translate', function ($phrase, $defaultLabel ,$params = []) {
 //                        return \Helpers\translate::translate($phrase, $defaultLabel ,$params);
 //                    })
-//                );       
-                
-        foreach ( $config['extensions'] as $extension)        
-            $this->twig->addExtension(new $extension());         
-                
-    }
-    
-    public function render($view, $context){
-        return $this->twig->render( $view,  $context);
+//                );
+
+        foreach ($config['extensions'] as $extension) {
+            $this->twig->addExtension(new $extension());
+        }
     }
 
+    public function render($view, $context)
+    {
+        return $this->twig->render($view, $context);
+    }
 }
